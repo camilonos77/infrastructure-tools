b8592684244719f7a1ab1cc828ccf78552c2641d
Sergio Barbosa <sbarbosa115@gmail.com>
2014-02-11 13:07:19 -0500

    Add remote history

---
+++ batch/upload_history_s3.bat
@@ -0,0 +1,3 @@
+::Upload result command to s3
+::author Sergio Barbosa <sbarbosa115@gmail.com>
+C:\"Program Files"\Amazon\AWSCLI\aws.exe s3 mv G:\Files\HistoryStatistics s3://archive.publicar.com/HistoryStatistics/
