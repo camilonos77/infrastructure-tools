d3c32f742799bc734713761f1d2ce770e07c8617
Sergio Barbosa <sbarbosa115@gmail.com>
2014-02-11 10:43:21 -0500

    Corraction

--- batch/sync_user_files_s3.bat
+++ batch/sync_user_files_s3.bat
@@ -1,3 +1,3 @@
 ::Move Backups databases to AWS s3
 ::author Sergio Barbosa <sbarbosa115@gmail.com>
-C:\"Program Files"\Amazon\AWSCLI\aws.exe s3 mv s3://archive.publicar.com/LoadClicksStatistics/ G:\Files\LoadClicksStatistics --exclude '*' --include '*.xml' --recursive
+C:\"Program Files"\Amazon\AWSCLI\aws.exe s3 mv s3://archive.publicar.com/LoadClicksStatistics/ C:\Files\LoadClicksStatistics --recursive --exclude "*" --include "*.xml"
